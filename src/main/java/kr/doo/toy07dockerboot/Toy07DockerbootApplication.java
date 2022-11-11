b05564e9aa7395493aec963c4b60e10c34dd103b
vincent.oh <lawsnland@gmail.com>
2022-11-11 16:44:15 +0900

    Initial commit

---
+++ src/main/java/kr/doo/toy07dockerboot/Toy07DockerbootApplication.java
@@ -0,0 +1,13 @@
+package kr.doo.toy07dockerboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy07DockerbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Toy07DockerbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/doo/toy07dockerboot/Toy07DockerbootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.doo.toy07dockerboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Toy07DockerbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
